da1f50b39b0170d1bede2ff075605f117a0a7a80
Joshua Girard <joshuagirard1@gmail.com>
2012-02-15 18:04:13 -0700

    Initial Commit

---
+++ ProxyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProxyApp
+//
+//  Created by Joshua Girard on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
