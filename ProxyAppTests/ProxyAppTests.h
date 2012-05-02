d41180217314d569d143966fd4ce9cdb544c143c
Joshua Girard <joshuagirard1@gmail.com>
2012-05-01 19:32:36 -0700

    Revert "Cleanup"
    This reverts commit 8569daa8b886ad2a352b8b7248d2d09444d0bc8f.

---
+++ ProxyAppTests/ProxyAppTests.h
@@ -0,0 +1,13 @@
+//
+//  ProxyAppTests.h
+//  ProxyAppTests
+//
+//  Created by Joshua Girard on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ProxyAppTests : SenTestCase
+
+@end
